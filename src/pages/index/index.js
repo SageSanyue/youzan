ad5b98a7e9bdb9bfe1851af490d3b105c48b23aa
Sanyue2333 <562441461@qq.com>
2018-07-06 00:13:26 +0800

    VUE重构底部菜单栏

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,19 +5,49 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
-console.log(axios)
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll)
+
+//console.log(axios)
+import Foot from 'components/Foot.vue'
 
 let app = new Vue({
     el: '#app',
     data: {
-        lists: null
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false,
+        allLoaded: false
     },
     created(){
-        axios.post(url.hotLists,{
-            pageNum: 1,
-            pageSize: 6
-        }).then(res => {
-            this.lists = res.data.lists
-        })
+        this.getLists()
+    },
+    methods: {
+        getLists(){
+            if(this.allLoaded)return
+            this.loading = true
+            axios.post(url.hotLists,{
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+            }).then(res => {
+                let curLists = res.data.lists
+                //判断所有数据是否加载完毕
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    //第一次请求数据
+                    this.lists = curLists
+                }
+                this.loading = false
+                this.pageNum ++
+            })
+        }
+    },
+    components: {
+        Foot
     }
 })

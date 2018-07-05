0d8bab51edc8845cb020fe447d0b11af68510f6e
Sanyue2333 <562441461@qq.com>
2018-07-05 20:35:24 +0800

    数据获取和渲染

---
+++ src/modules/js/api.js
@@ -0,0 +1,14 @@
+let url = {
+    hotLists: '/index/hotLists'
+}
+ 
+//开发环境（用的ip接口与后端协商后单独开发）和真实环境(映射到相应后台)的切换
+let host = 'http://rap2api.taobao.org/app/mock/7058'
+
+for(let key in url){
+    if(url.hasOwnProperty(key)){
+        url[key] = host + url[key]
+    }
+}
+
+export default url

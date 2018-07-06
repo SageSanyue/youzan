d8065a47d34aa7158a5b7ff91e2df4e4f7762206
Sanyue2333 <562441461@qq.com>
2018-07-06 19:27:22 +0800

    分类页面侧边栏mock数据加载

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,6 +1,7 @@
 let url = {
     hotLists: '/index/hotLists',
-    banner: '/index/banner'
+    banner: '/index/banner',
+    topList: '/category/topList'
 }
  
 //开发环境（用的ip接口与后端协商后单独开发）和真实环境(映射到相应后台)的切换
--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,2 +1,30 @@
 import 'css/common.css'
 import './category.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+
+import Foot from 'components/Foot.vue'
+
+new Vue({
+    el: '#app',
+    data: {
+        topLists: null
+    },
+    created(){
+        this.getTopList()
+    },
+    methods: {
+        getTopList(){
+            axios.post(url.topList).then(res => {
+                this.topLists = res.data.lists
+            }).catch(res => {
+
+            })
+        }
+    },
+    components: {
+        Foot
+    }
+})

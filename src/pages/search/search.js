c78d9dcccc4dfe3f08bcb7847908de4baf8e4e79
Sanyue2333 <562441461@qq.com>
2018-07-08 18:29:48 +0800

    给'分类'页面的'食品酒水'-'水果'添加滑动'回到顶部'

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -9,12 +9,14 @@ import qs from 'qs'
 let {keyword, id} = qs.parse(location.search.substr(1))
 
 import mixin from 'js/mixin.js'
+import Velocity from 'velocity-animate'
 
 new Vue({
     el: '.container',
     data: {
         searchList: null,
-        keyword
+        keyword,
+        isShow: false
     },
     created(){
         this.getSearchList()
@@ -24,6 +26,16 @@ new Vue({
             axios.post(url.searchList,{keyword,id}).then(res => {
                 this.searchList = res.data.lists
             })
+        },
+        move(){
+            if(document.scrollingElement.scrollTop > 100) {
+                this.isShow = true
+              } else {
+                this.isShow = false
+              }
+        },
+        toTop(){
+              Velocity(document.body, 'scroll', {duration: 1000})
         }
     },
     mixins: [mixin]

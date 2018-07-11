dbb3c3f5c55dc46b602d3e6a01d4defd4d611aa2
Sanyue2333 <562441461@qq.com>
2018-07-11 16:09:19 +0800

    rap接口不能用https

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -21,7 +21,7 @@ let url = {
 }
  
 //开发环境（用的ip接口与后端协商后单独开发）和真实环境(映射到相应后台)的切换
-let host = 'https://rap2api.taobao.org/app/mock/7058'
+let host = '//rap2api.taobao.org/app/mock/7058'
 
 for(let key in url){
     if(url.hasOwnProperty(key)){

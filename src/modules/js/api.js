15fa748215b840f11d4856804b1ba7b7fe7958ab
Sanyue2333 <562441461@qq.com>
2018-07-12 08:37:06 +0800

    取消浏览器的同源

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -15,13 +15,13 @@ let url = {
     cartUpdate: '/cart/update',
     addressLists: '/address/list',
     addressAdd: '/address/add',
-    addressRemove: 'address/remove',
-    addressUpdate: 'address/update',
-    addressSetDefault: 'address/setDefault'
+    addressRemove: '/address/remove',
+    addressUpdate: '/address/update',
+    addressSetDefault: '/address/setDefault'
 }
  
 //开发环境（用的ip接口与后端协商后单独开发）和真实环境(映射到相应后台)的切换
-let host = '//rap2api.taobao.org/app/mock/7058'
+let host = 'http://rap2api.taobao.org/app/mock/7058'
 
 for(let key in url){
     if(url.hasOwnProperty(key)){
